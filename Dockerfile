f29244ff6ad52798d0a5071546aada3d62791af5
Robert Mikes <robertm@titusgt.com>
2023-09-26 09:30:39 +0000

    archive updates done a while ago

--- Dockerfile
+++ Dockerfile
@@ -2,25 +2,28 @@
 # Docker image for Pentaho Server Community Edition.
 #
 
+FROM openjdk:8
 
 # Set maintainer
 MAINTAINER Robert Mikes <robert@mikes.hu>
 
 # Set environment variables
-ENV BISERVER_VERSION=9.1.0-324
+ENV SERVER_VERSION=9.1.0.0-324
 
 # Set label
-LABEL java_server="Pentaho Server $BISERVER_VERSION Community Edition"
+LABEL java_server="Pentaho Server $SERVER_VERSION Community Edition"
 
+COPY foo pentaho-server-ce-* /opt/
 RUN cd /opt \
-    && wget --timeout=10 --waitretry=2 --tries=50 --retry-connrefused --progress=dot:giga https://sourceforge.net/projects/pentaho/files/Pentaho%209.1/server/pentaho-server-ce-9.1.0.0-324.zip \
-    && ls -l \
+    && (ls -l pentaho-server-ce-${SERVER_VERSION}.zip && echo 'NO NEED TO DOWNLOAD') || wget --timeout=10 --waitretry=2 --tries=50 --retry-connrefused --progress=dot:giga https://sourceforge.net/projects/pentaho/files/Pentaho%209.1/server/pentaho-server-ce-${SERVER_VERSION}.zip \
     && unzip *.zip \
     && rm *.zip \
     && cd pentaho-server \
     && rm promptuser.sh \
-    && ./set-pentaho-env.sh
+    && sed -i 's/startup.sh/catalina.sh run/' start-pentaho.sh
 
-#EXPOSE 8080
+EXPOSE 8080
 
-CMD ["/sbin/my_init", "--", "./start-pentaho.sh"]
+WORKDIR /opt/pentaho-server
+ENTRYPOINT ./start-pentaho.sh
+
--- build.sh
+++ build.sh
@@ -1,4 +1,56 @@
 #!/bin/bash
 
-docker build --tag taurus227/docker-biserver-ce:9.1 .
-docker container run --detach --name bi2 --publish 8081:8081 taurus227/docker-biserver-ce:9.1
+display() {
+    echo
+    echo ============================================================
+    echo NEXT:
+    echo -e "\033[01;34m$(pwd)\033[00m$ $@"
+    echo
+}
+
+confirm() {
+    display "$@"
+
+    read -r -p"Run? [Y/n] ";echo;echo
+    if [[ $REPLY =~ ^[Yy]?$ ]]
+    then
+        echo -e "Running... \033[01;34m$(pwd)\033[00m$ $@"
+        echo
+        run "$@"
+    fi
+}
+
+disprun() {
+    display "$@"
+    run "$@"
+}
+
+run() {
+        exitOnError='true'
+        if [[ $1 == 'DONTEXITONERROR' ]]
+        then
+            exitOnError='false'
+            echo "Exit on error? ${exitOnError}"
+            shift;
+        fi
+
+        "$@"
+
+        rc=$?
+        echo
+        echo ============================================================
+
+        if [[ $rc != 0 ]]
+        then
+            echo
+            echo "ERROR CODE ${rc} WHEN EXECUTING IN `pwd` THE COMMAND: $@"
+            echo
+            if [[ ${exitOnError} == 'true' ]]
+            then
+                exit $rc
+            fi
+        fi
+}
+
+disprun docker build --tag taurus227/pentaho-server-ce:9.1 .
+disprun docker container run --detach --name pentaho --publish 8080:8080 taurus227/pentaho-server-ce:9.1
